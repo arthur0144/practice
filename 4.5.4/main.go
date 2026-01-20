2514a961547b8bccdf75ad46b77936a473311f2a
arthur0144 <arthurdanilevsky@gmail.com>
2026-01-20 08:17:55 +0300

    schedule task

---
+++ 4.5.4/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// начало решения
+//Напишите функцию schedule(), которая запускает регулярное выполнение задачи:
+//
+//Принимает на входе интервал времени dur и функцию fn.
+//Возвращает функцию отмены cancel.
+//После запуска каждые dur времени выполняет fn.
+//Если клиент вызвал cancel() — перестает выполнять fn.
+
+func schedule(dur time.Duration, fn func()) func() {
+	ticker := time.NewTicker(dur)
+	cc := make(chan struct{}, 1)
+
+	go func() {
+		defer ticker.Stop()
+		for {
+			select {
+			case <-ticker.C:
+				fn()
+			case <-cc:
+				return
+			}
+		}
+	}()
+
+	return func() {
+		if ticker.C == nil {
+			return
+		}
+		cc <- struct{}{}
+	}
+}
+
+// конец решения
+
+func main() {
+	work := func() {
+		at := time.Now()
+		fmt.Printf("%s: work done\n", at.Format("15:04:05.000"))
+	}
+
+	cancel := schedule(50*time.Millisecond, work)
+	cancel()
+
+	// хватит на 5 тиков
+	time.Sleep(260 * time.Millisecond)
+}
